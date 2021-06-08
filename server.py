496bf6fce6448263dbe800cd4fc901cf3a88c675
Robert <roberttrzaska@outlook.com>
2021-06-08 10:27:26 +0200

    time

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
 
 @app.route("/hello/")
 def hello():
-    name = "Robert"
-    return "Witaj użytkowniku " + name + "!"
+    local = datetime.now()
+    return local.strftime("%H:%M:%S")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
